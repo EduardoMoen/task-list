4e58ff26ad8aa9c3f4587ccea5b92153107a2a14
Eduardo Doerner <eduardodoerner@gmail.com>
2025-12-18 16:40:39 -0300

    add Tags list view

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,9 @@
-from .views import index
 from django.urls import path
+from .views import index, TagListView
 
 urlpatterns = [
     path('', index, name='index'),
+    path("tags/", TagListView.as_view(), name='tag-list'),
 ]
 
 app_name = 'task'
--- task/views.py
+++ task/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+
+from task.models import Tag
 
 
 def index(request):
     return render(request,'task/index.html')
+
+
+class TagListView(generic.ListView):
+    model = Tag
