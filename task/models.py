5b41c1ccf64eb90199d7fc887f7d348afe7cf60e
Eduardo Doerner <eduardodoerner@gmail.com>
2025-12-18 16:07:29 -0300

    add admin models sidebar home page

--- task/models.py
+++ task/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    date = models.DateField()
+    deadline = models.DateTimeField(null=True, blank=True)
+    concluded = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='tasks'
+    )
+
+    def __str__(self):
+        return self.content
